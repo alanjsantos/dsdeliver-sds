83f8dbb53eb37a7477ccd73940fbfcfe244325b6
Santos <alanjosedossantos011@gmail.com>
2021-01-11 21:59:38 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controller/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controller/OrderController.java
@@ -3,8 +3,11 @@ package com.devsuperior.dsdeliver.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,13 @@ public class OrderController {
 		List<OrderDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+		
 	}
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/service/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entity.Order;
+import com.devsuperior.dsdeliver.entity.OrderStatus;
+import com.devsuperior.dsdeliver.entity.Product;
 import com.devsuperior.dsdeliver.repository.OrderRepository;
+import com.devsuperior.dsdeliver.repository.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,11 +22,35 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
-	@Transactional(readOnly = true)
-	public List<OrderDTO> findAll(){
-		List<Order> list = repository.findOrderWithProducts();
-		
-		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); // convertendo Product para uma lista ProductDTO
+	@Autowired
+	private ProductRepository productsRepository;
 
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrderWithProducts();
+
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); // convertendo Product para uma
+																						// lista ProductDTO
+
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		
+		//List de produtos (varrendo todos os produos.)
+		for (ProductDTO p : dto.getProducts()) {
+			//associando cada produto ao pedido: 
+			//1. Instanciando uma enttidade correspondente a cada DTO.
+			//2. injetar a dependencia ProductsRepository
+			//3 Instanciar Product recebendo = productsRepository.getOne(p.getId()) (getOne() vai instanciar um produto, porém ele nao vai no banco de dados. Ele salva as assocações dos produtos que estão nos pedidos.)
+			Product product = productsRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		
+		return new OrderDTO(order);
 	}
 }
