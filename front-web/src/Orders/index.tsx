88ba3d1b1b165f6f0d3403bc110463c3a194911c
Santos <alanjosedossantos011@gmail.com>
2021-01-14 18:50:51 -0300

    Add Routes and StepsHJeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import StepsHeader from './StepsHeader';
+
+function Orders() {
+    return (
+       <div className="orders-container">
+           <StepsHeader />
+           <h1></h1>
+
+       </div>
+    )
+}
+
+export default Orders;
